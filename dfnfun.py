1c51cb12c67387ebd26acb022600aa633db3a511
manjupoo <47881058+manjupoo@users.noreply.github.com>
2019-03-08 11:35:46 +0530

    Add files via upload

---
+++ dfnfun.py
@@ -0,0 +1,14 @@
+def fun1():
+	print("hi")
+	print("hello")
+fun1()
+
+def fun2(a,b):
+	d=a*b
+	print("mul of",a,"and",b,"is",d)
+fun2(2,10)
+
+def fun3(m="mython"):
+	print(m)
+fun3("byeeeee")
+fun3()
