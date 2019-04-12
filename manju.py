1bcae6bf6f8a428c2b1952c9b650e29f47afcae1
manjupoo <47881058+manjupoo@users.noreply.github.com>
2019-04-12 12:28:42 +0530

    Add files via upload

--- manju.py
+++ manju.py
@@ -1,11 +1,45 @@
-import random
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
 
-while True:
-		d=input("press r to roll and q to quit")
-		if d=="r":
-		 	print("You got: ",random.randint(1,6))
-		if d=="q":
-		 	print("Bye!")
-		 	exit()
+try:
+      GPIO.setmode(GPIO.BOARD)
 
-print("end")
+      PIN_TRIGGER = 40
+      PIN_ECHO = 38
+
+      GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+      GPIO.setup(PIN_ECHO, GPIO.IN)
+
+      GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+      print ("Waiting for sensor to settle")
+
+      time.sleep(2)
+
+      print ("Calculating distance")
+
+      GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+
+      time.sleep(0.00001)
+
+      GPIO.output(PIN_TRIGGER, GPIO.LOW)
+      dist =distance()
+      print("measured distance=",dist)
+      if (dist<50):
+          os.system("fswebcam -S 20 -r 1280X720 --no-banner DATE.jpg")
+            
+ 
+         # Reset by pressing CTRL + C 
+      while GPIO.input(PIN_ECHO)==0:
+          pulse_start_time = time.time()
+      while GPIO.input(PIN_ECHO)==1:
+          pulse_end_time = time.time()
+      pulse_duration = pulse_end_time - pulse_start_time
+      distance = round(pulse_duration * 17150, 2)
+      print ("Distance:",distance,"cm")
+
+finally:
+    
+      GPIO.cleanup()
+
